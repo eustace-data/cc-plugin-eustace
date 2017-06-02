a4d1d6b873600200c4527bbd3b5923050aa0cabc
agstephens <ag.stephens@stfc.ac.uk>
2017-06-02 15:58:45 +0100

    First setup of EUSTACE test code - integrated with Vocabs.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# cc-plugin-eustace
-EUSTACE Compliance Checker plugin
+# EUSTACE Compliance Checker Plugin
+
+This is a checker for EUSTACE files.
+
+It works with the `agstephens` fork of the [ioos/compliance-checker](https://github.com/agstephens/compliance-checker) which includes an interface to the [pyessv library](https://github.com/ES-DOC/pyessv).
+
+### Usage
+
+Activate your virtualenv/conda env and `pip install git+git://github.com/eustace-test/cc-plugin-eustace`.  You should see `eustace` in the list of `--test` suites when running `compliance-checker`.
+
+### Setup instructions
+
+...to come soon...
+
+### Running with new EUSTACE tests
+
+We have defined `eustace-core` and `eustace-station` tests. You can run both of these with:
+
+`compliance-checker --test eustace-core --test eustace-station ./cc_plugin_eustace/tests/data/eustace/test_cdl_global_atts.nc`
+
+### Development
+
+Copied the `cc-plugin-glider` code and converted to use with EUSTACE.
+
+For development use you need pip to install the plugin. Can do this with local copy using:
+
+ `pip install -e .`
+
+Note: adding any new modules will require this `pip` command to be re-run.
---
+++ setup.py
@@ -0,0 +1,40 @@
+from __future__ import with_statement
+import sys
+
+from setuptools import setup, find_packages
+
+from cc_plugin_eustace import __version__
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+reqs = [line.strip() for line in open('requirements.txt')]
+
+setup(name                 = "cc-plugin-eustace",
+    version              = __version__,
+    description          = "Compliance Checker EUSTACE plugin",
+    long_description     = readme(),
+    license              = 'BSD License',
+    author               = "Ag Stephens",
+    author_email         = "ag.stephens@stfc.ac.uk",
+    url                  = "https://github.com/eustace-test/cc-plugin-eustace",
+    packages             = find_packages(),
+    install_requires     = reqs,
+    classifiers          = [
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD Software License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering',
+        ],
+    entry_points         = {
+        'compliance_checker.suites': [
+            'eustace-core = cc_plugin_eustace.eustace_core:EUSTACECoreCheck',
+            'eustace-station = cc_plugin_eustace.eustace_station:EUSTACEStationCheck'
+        ]
+    }
+)
+
