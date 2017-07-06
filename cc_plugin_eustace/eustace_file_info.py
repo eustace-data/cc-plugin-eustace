040cdf82fbc7c1f13428190ecaf8268fb6087394
agstephens <ag.stephens@stfc.ac.uk>
2017-07-06 11:21:23 +0100

    Updated plugins.

---
+++ cc_plugin_eustace/eustace_file_info.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""
+cc_plugin_eustace.eustace_file_info
+
+Compliance Test Suite: Check file names and external information about EUSTACE files
+"""
+
+import os
+from netCDF4 import Dataset
+
+# Import base objects from compliance checker
+from compliance_checker.base import Result, GenericFile
+
+# Restrict which vocabs will load (for efficiency)
+os.environ["ESSV_VOCABS_ACTIVE"] = "eustace-team"
+
+# Import checklib
+import checklib.register.file_checks_register as check_package
+
+class EUSTACEFileInfoCheck(object):
+    register_checker = True
+    name = 'eustace-file-info'
+    supported_ds = [GenericFile, Dataset]
+
+    @classmethod
+    def make_result(cls, level, score, out_of, name, messages):
+        return Result(level, (score, out_of), name, messages)
+
+    def setup(self, fpath):
+        pass
+
+    
+    def check_fi01(self, ds):
+        return check_package.FileSizeCheck(kwargs={'threshold': '2', 'strictness': 'soft'},
+                                                    level="MEDIUM",
+                                                    vocabulary_ref="")(ds.filepath())
+    
+    def check_fi02(self, ds):
+        return check_package.FileSizeCheck(kwargs={'threshold': '4', 'strictness': 'hard'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="")(ds.filepath())
+    
+    def check_fi03(self, ds):
+        return check_package.FileNameStructureCheck(kwargs={'delimiter': '_', 'extension': '.nc'},
+                                                    level="HIGH",
+                                                    vocabulary_ref="")(ds.filepath())
+    
